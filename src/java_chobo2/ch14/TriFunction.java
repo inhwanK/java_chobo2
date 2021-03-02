2127fc1263fd45465538fffc6e5f3d7237720774
inhwanK <inhan1009@naver.com>
2021-03-02 12:40:34 +0900

    3월 2일 수업

---
+++ src/java_chobo2/ch14/TriFunction.java
@@ -0,0 +1,7 @@
+package java_chobo2.ch14;
+
+@FunctionalInterface
+public interface TriFunction<T,U,V,R> {
+	R apply(T t, U u, V v);
+}
+
---
+++ src/java_chobo2/ch14/TriFunctionEX.java
@@ -0,0 +1,10 @@
+package java_chobo2.ch14;
+
+public class TriFunctionEX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
